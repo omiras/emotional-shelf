4c713a7684c618d26c926fc119aaf7b2def2ccf4
omiras <oscar.miras@gmail.com>
2024-09-23 21:41:13 +0200

    limit books

--- controllers/book.controllers.js
+++ controllers/book.controllers.js
@@ -2,7 +2,7 @@ const Book = require('../models/book.model');
 
 // otra forma de exportar varios recursos, ¿cuál os gusta más?
 exports.getBooks = async (req, res) => {
-    const books = await Book.find();
+    const books = await Book.find().limit(20);
 
     return res.status(200).json({
         message: "Query executed successfully",
