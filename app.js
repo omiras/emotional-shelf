06d8c5ed25a4946bfd60403324ed3f8e91b13a0e
VanyaMark <markova.vanya@gmail.com>
2024-09-24 19:01:56 +0200

    added model schema

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const express = require('express');
 const app = express();
 
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (req, res) => {
+    connectDB();
     console.log("Servidor escuchando correctamente en el puerto " + PORT);
 });
+
+
+
+// const bookRoutes = require('./routes/book.routes');
+// app.use('/api', bookRoutes);
+
+
+
---
+++ models/book.model.js
@@ -0,0 +1,32 @@
+const {Schema, model} = require('mongoose');
+
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        maxLength: 30,
+    },
+    isbn: {
+        type: String,
+        required: true,
+        match: /^[0-9]{13}$/,
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 4000 // Amazon Kingle Direct Publishing
+    },
+    emotions: {
+        type: [String],
+        required: true,
+        // TODO: Cómo aún no tengo que crear libros voy a dejar por el momento otras validaciones y consideraciones por tal de acabar la iteración 1
+    }
+})
+
+const Book = model('book', bookSchema);
+
+module.exports = Book;
