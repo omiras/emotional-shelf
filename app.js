d6e38e9ccd66902836894b6548bd331e9c4a484f
VanyaMark <markova.vanya@gmail.com>
2024-09-24 18:38:21 +0200

    added dependencies

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 
 const app = express();
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (req, res) => {
     console.log("Servidor escuchando correctamente en el puerto " + PORT);
