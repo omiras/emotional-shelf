274c867c57b0657c97fd5e4350ccaaa9f370ae80
VanyaMark <markova.vanya@gmail.com>
2024-09-26 22:35:04 +0200

    normalized emotion param according the book model

--- controllers/book.controllers.js
+++ controllers/book.controllers.js
@@ -23,11 +23,13 @@ exports.getBooks = async (req, res) => {
 
 exports.getBooksByEmotion = async (req, res) => {
   const { emotion } = req.params;
+  //Converting the parameter to match the model's format, ensuring consistent handling of emotions regardless of input case variations
+  let modifiedEmotion = emotion.charAt(0).toUpperCase() + emotion.slice(1).toLowerCase();
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     return res.status(400).json({
-      message: `Error when trying to find books with emotion ${emotion}`,
+      message: `Error when trying to find books with emotion ${modifiedEmotion}`,
       errors: errors.array(),
     });
   }
@@ -39,9 +41,9 @@ exports.getBooksByEmotion = async (req, res) => {
   //     });
   // }
   try {
-    const books = await Book.find({ emotions: emotion }).limit(20);
+    const books = await Book.find({ emotions: modifiedEmotion }).limit(20);
     return res.status(200).json({
-      message: `Query by the emotion: ${emotion} executed successfully`,
+      message: `Query by the emotion: ${modifiedEmotion} executed successfully`,
       results: books,
     });
   } catch (error) {
@@ -51,22 +53,24 @@ exports.getBooksByEmotion = async (req, res) => {
 
 exports.getRandomRecommendationByEmotion = async (req, res) => {
     const { emotion } = req.params;
+    let modifiedEmotion = emotion.charAt(0).toUpperCase() + emotion.slice(1).toLowerCase();
+    console.log(modifiedEmotion)
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(400).json({
-        message: `Error when trying to find a random book with emotion ${emotion}`,
+        message: `Error when trying to find a random book with emotion ${modifiedEmotion}`,
         errors: errors.array(),
       });
     }
     try {
         const randomBook = await Book.aggregate([
-            { $match: { emotions: { $in: [emotion] } } },  // Match books where the emotions array contains the given emotion
+            { $match: { emotions: { $in: [modifiedEmotion] } } },  // Match books where the emotions array contains the given emotion
             { $sample: { size: 1 } }                       // Randomly select one book from the matched books
         ]);
 
         return res.status(200).json({
-          message: `Random book by the emotion: ${emotion} retrieved successfully`,
+          message: `Random book by the emotion: ${modifiedEmotion} retrieved successfully`,
           results: randomBook[0],
         });
       } catch (error) {
--- routes/book.validations.js
+++ routes/book.validations.js
@@ -2,4 +2,12 @@ const { param } = require('express-validator');
 
 const allowedEmotions = ['Inspiration', 'Curiosity', 'Escapism', 'Nostalgia', 'Happiness', 'Sadness'];
 
-exports.validateEmotion = [param('emotion').isIn(allowedEmotions).withMessage('Invalid emotion')];
+exports.validateEmotion = [
+    param('emotion')
+        .customSanitizer((value) => {
+            // Normalize the input here
+            return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+        })
+        .isIn(allowedEmotions)
+        .withMessage('Invalid emotion')
+];
