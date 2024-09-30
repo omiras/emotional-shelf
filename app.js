35290885008d47f95247558759b634b8ed8b7816
omiras <oscar.miras@gmail.com>
2024-09-30 18:23:31 +0200

    post done

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
 
 app.use('/api', bookRoutes);
 
