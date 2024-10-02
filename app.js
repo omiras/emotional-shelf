4781a0366af97a6ee4d0174ca318cb9f9166c397
omiras <oscar.miras@gmail.com>
2024-10-02 10:35:39 +0200

    fetch example from the same server

--- app.js
+++ app.js
@@ -15,7 +15,20 @@ app.use((req, res, next) => {
     console.log(`${new Date().toISOString()} Me acaban de hacer una petición a ${req.url}`);
     next();
 });
+
+// Permitir peticiones des de cualquier máquina que no sea la misma des de donde se levanta esta REST API
+app.use((req, res, next) => {
+    // Permite que las peticiones des de un script des de cualquier origen
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 app.use(express.json());
+
+app.get('/show-books', (req, res) => {
+    res.sendFile(__dirname + '/show-books.html');
+})
+
 app.use('/api', bookRoutes);
 
 const PORT = process.env.PORT || 3000;
